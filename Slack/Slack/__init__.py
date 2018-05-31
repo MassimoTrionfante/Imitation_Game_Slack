ea7d7f1f1959fc7fd9607ec57d94acbc31d92a03
MassimoTrionfante <massimo.trionfante@studenti.uniparthenope.it>
2018-05-31 18:11:50 +0200

    Update

--- Slack/Slack/__init__.py
+++ Slack/Slack/__init__.py
@@ -1,11 +1,23 @@
+# UTENTI CONTENUTI IN db.Lasck.utenti
+# CHAT CONTENUTA IN db.Lasck.chat
+
 import os
+import pprint
 import pymongo
-from flask import Flask
-from flask import render_template
-from . import db
+from flask import Flask, flash, render_template, redirect, request, session, url_for, jsonify
+from random import randint
+from pymongo import MongoClient
+
+#Collect database method
+def get_db():
+  client = MongoClient('localhost',27017)
+  db = client.Lasck
+  return db
+
 
 def create_app(test_config=None):
   app = Flask(__name__,instance_relative_config=True)
+  app.config.from_mapping(SECRET_KEY='dev')
 
   if test_config is None:
     app.config.from_pyfile('config.py',silent=True)
@@ -17,37 +29,79 @@ def create_app(test_config=None):
   except OSError:
     pass
 
-  @app.route('/')
+  #Main page
+  @app.route('/', methods=['GET','POST'])
   def index():
+    if request.method=='POST':
+      return redirect(url_for('register'))
     return render_template('index.html')
 
-  @app.route('/signin')
+  #Sign in form
+  @app.route('/signin', methods=['GET','POST'])
   def signIn():
+    if request.method=='POST':
+      email=request.form['email']
+      workspace=request.form['workspace']
+      password=request.form['password']
+
+      error=None
+      db=get_db()
+      user = db.utenti.find_one({'email':email})
+      work = db.utenti.find_one({'workspace':workspace})
+      documento = db.utenti.find_one({'email':email,'workspace':workspace})
+      checkpwd = db.utenti.find_one({'email':email,'workspace':workspace,'password':password})
+      #Check that user is registered in Lasck + check that workspace exists
+      if user is None:
+        error= "ERROR: User does not exist!"
+      elif work is None:
+        error= "ERROR: Workspace does not exist!"
+      #Check that user is inside that workspace
+      elif documento is None:
+        error= "ERROR: User isn't binded to that workspace!"
+      #Check that user nailed password
+      elif checkpwd is None:
+        error= "ERROR: Password is incorrect!"
+
+      if error is None:
+        return "Ok" #TODO link to appropriate chat screen
+     
+      flash(error)
     return render_template('signin.html')
 
-  @app.route('/signindomain')
-  def signInDomain():
-    return render_template('signindomain.html')
+  #Sign in form 2
+ # @app.route('/signindomain')
+ # def signInDomain():
+ #   return render_template('signindomain.html')
 
+  #Main chat
   @app.route('/main')
   def main():
     return render_template('main.html')
 
-  @app.route('/register')
-  def register():
+  #Methods for creating a workspace
+  #Form with mail
+  @app.route('/register', methods=['GET','POST'])
+  def register():    
+    if request.method=='POST':
+      email=request.form['email'] #Get various input fields
+      username=request.form['username']
+      nick=request.form['nick']
+      workspace=request.form['workspace']
+      password=request.form['password']
+      #insert in database
+      db = get_db()
+      db.utenti.insert({"email":email, "username":username,"workspace":workspace, "nick":nick, "password":password})
+      print("Added following document:")
+      pprint.pprint(db.utenti.find_one({"email":email,"workspace":workspace}))
+      return redirect(url_for('signIn'))
     return render_template('register.html')
-
-  @app.route('/insert')
-  def insert():
-    database = db.getDB()
-    database = db.addDocument(database.collezione,{"nome":"dio"})    
-    return "Documento aggiunto."    
+ 
 
   @app.route('/resetDB')
   def nuke():
-    database = db.getDB()
-    database = db.deleteAll(database.collezione)
-    return "Collezione eliminata!"
+    db = get_db()
+    #db.users.remove({})
+    return "Collezioni eliminate!"
 
 
   return app
